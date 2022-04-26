3c585bc0fd167137de6b4c79358fe5f0cdd38f83
Scropy <1olozturk09@gmail.com>
2022-04-26 21:00:54 +0300

    Code cleaned

--- src/main/java/com/github/scropytr/legendinventoryapi/InventoryAPI.java
+++ src/main/java/com/github/scropytr/legendinventoryapi/InventoryAPI.java
@@ -2,6 +2,7 @@ package com.github.scropytr.legendinventoryapi;
 
 import com.github.scropytr.legendinventoryapi.listeners.GUIListener;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 
 public class InventoryAPI {
